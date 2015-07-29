d0f4df87e3300de18d3facf827e473297689869a
jypuigbo <jordiysard@gmail.com>
2015-07-29 19:53:34 +0200

    [drawingNao] Missing magic drawing

--- drawingNao/almotion_draw.py
+++ drawingNao/almotion_draw.py
@@ -2,7 +2,8 @@
 
 import time
 import argparse
-from naoqi import ALProxy
+from naoqi import ALProxy, ALModule, ALBroker
+import argparse
 from OSC import OSCServer, OSCClient
 import sys
 import motion
@@ -23,6 +24,7 @@ p = [p3]#[p0,p1,p2,p3]
 ReactToTouch = None
 memory = None
 go_to_center = False
+global_time = 0.0
 class ReactToTouch(ALModule):
     """ A simple module able to react
         to touch events.
@@ -38,6 +40,7 @@ class ReactToTouch(ALModule):
         memory.subscribeToEvent("TouchChanged",
             "ReactToTouch",
             "onTouched")
+        self.tts = ALProxy("ALTextToSpeech")
 
     def onTouched(self, strVarName, value):
         """ This will be called each time a touch
@@ -49,12 +52,17 @@ class ReactToTouch(ALModule):
         memory.unsubscribeToEvent("TouchChanged",
             "ReactToTouch")
 
+        print 'someone touched my head!'
         touched_bodies = []
         for p in value:
             if p[1]:
                 touched_bodies.append(p[0])
         global go_to_center
-        go_to_center = True
+        global global_time
+        if time.time()-global_time>1.25:
+            go_to_center = True
+            sentences = ["Ouch!", "I'm sorry", "mmm..."]
+            self.tts.say(random.sample(sentences,1)[0])
 
         # Subscribe again to the event
         memory.subscribeToEvent("TouchChanged",
@@ -73,6 +81,14 @@ def move(addr, tags, data, source):
 
 
 def main(robotIP, PORT = 9559):
+    myBroker = ALBroker("myBroker", #needed for subscribers and to construct naoqi modules
+        "0.0.0.0",
+        0,
+        robotIP,
+        PORT)
+    global ReactToTouch, go_to_center, global_time
+    
+
     global motionProxy, jointNames
     
     motionProxy = ALProxy("ALMotion", robotIP, PORT)
@@ -100,7 +116,7 @@ def main(robotIP, PORT = 9559):
     # Define the changes relative to the current position
     dx         =  0.03      # translation axis X (meters)
     dy         =  -0.04     # translation axis Y (meters)
-    dz         =  0.02      # translation axis Z (meters)
+    dz         =  0.03      # translation axis Z (meters)
     dwx        =  0.00      # rotation axis X (radians)
     dwy        =  0.00      # rotation axis Y (radians)
     dwz        =  0.00      # rotation axis Z (radians)
@@ -108,40 +124,97 @@ def main(robotIP, PORT = 9559):
 
     # Go to the target and back again
     path       = [targetPos]
-    times      = [2.0] # seconds
+    times      = [1.5] # seconds
 
     motionProxy.positionInterpolation(effector, space, path,
                                       axisMask, times, isAbsolute)
-    sz=0.00
-    n = input('# of trials?')
-    k=1.6
-    L=10
-    for i in range(n):
-        if random.random()<0.3:
-            k=max(1,k-0.15)
-        dx = k*random.random()*L-L/2 + center[0]
-        dy = k*random.random()*L-L/2 + center[1]
+    dz=0.00
+    input('Press 1 to begin: ')
+    k=1.5
+    L=0.12
+    n=10
+    speed = 0.15
+    #Initialize listener
+    ReactToTouch = ReactToTouch("ReactToTouch")
+
+    center = motionProxy.getPosition(effector, space, True)
+    try:
+      while 1:#for i in range(n):
+        speed = 0.15
+        # False learning
+        if random.random()<0.1:
+            k=max(0.9,k-0.15)
+
+        # Generate next target x,y
+        x = k*random.random()*L-L/2 + center[0]
+        y = k*random.random()*L-L/2 + center[1]
+        # Get current Position & define target position
+        currentPos = motionProxy.getPosition(effector, space, True)
+        targetPos = currentPos
+        targetPos[0] = x
+        targetPos[1] = y
+        targetPos[2] = center[2]
+        print 'new target'
+        print targetPos
+        # Move to position, being able to interrupt
+        motionProxy.setPosition(effector, space, targetPos, speed, axisMask)
         
-        targetPos  = [dx, dy, dz, dwx, dwy, dwz]
-        path       = [targetPos]
-        center[0] -= targetPos[0]
-        center[1] -= targetPos[1]
-        motionProxy.positionInterpolation(effector, space, path,
-                                      axisMask, times, isAbsolute)
-    print 'Done!'
+        now = time.time()
+        go_to_center = False
+        while time.time()-now < times[0] and not go_to_center:
+            time.sleep(0.1)
+        if go_to_center:
+            if random.random()<0.9:
+                k=max(0.9,k-0.15)
+            speed = 0.4
+            go_to_center = False
+            print 'going to center!'
+             # Get current Position & define target position
+            currentPos = motionProxy.getPosition(effector, space, True)
+            targetPos = currentPos
+            targetPos[0] = center[0]
+            targetPos[1] = center[1]
+            targetPos[2] = center[2]
+            print 'new target'
+            print targetPos
+            # Move to position, being able to interrupt
+            motionProxy.setPosition(effector, space, targetPos, speed, axisMask)
+            #Try it twice, because it doesn't seem to get there
+            '''currentPos = motionProxy.getPosition(effector, space, True)
+            motionProxy.positionInterpolation(effector, space, [currentPos],
+                                      axisMask, [0.51], isAbsolute)
+            targetPos  = [currentPos[0]-center[0], currentPos[1]-center[1], currentPos[2]-center[2], 0.0, 0.0, 0.0]
+            path       = [targetPos]
+            print targetPos
+            motionProxy.positionInterpolation(effector, space, path,
+                                      axisMask, [0.51], isAbsolute)
+            print "sleeping 2 seconds"'''
+            time.sleep(2.)
+            print "slept.."
+            global_time = time.time()
+
+        print 'out of the loop'
+        """else:
+            #currentPos = motionProxy.getPosition(effector, space, True)
+            #center[0] -= targetPos[0]
+            #center[1] -= targetPos[1]
+        """
+      print 'Done!'
+
     
     
 
-    server = OSCServer( ("" , 2222) )
-    server.addMsgHandler("/move", move)
-    try:
-        while 1:
+      server = OSCServer( ("" , 2222) )
+      server.addMsgHandler("/move", move)
+    
+      while 1:
             server.handle_request()
-            #print "after handle request"
     except KeyboardInterrupt:
         print "closing all OSC connections... and exit"
-        server.close() 
+        
         motionProxy.rest()
+        myBroker.shutdown()
+        server.close() 
 
 
     motionProxy.rest()
